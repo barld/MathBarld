2d61da41b30bfd18e8a83f5d2b6e40ea75d829d4
bakeds <bakeds@gmail.com>
2014-06-26 08:54:58 +0200

    add faculteit recusive and ituratiev

---
+++ MathBarld/Faculteit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBarld
+{
+    public static class Faculteit
+    {
+        static int GetIteratiev(int input)
+        {
+            int rtw = 1;
+            while(input > 0)
+            {
+                rtw *= input;
+                input--;
+            }
+            return rtw;
+        }
+
+        static int GetRecursive(int input)
+        {
+            if (input == 1) return 1;
+            return input * GetRecursive(input - 1);
+        }
+    }
+}
