7bd6cacaf99145d15de8a9a513a64ec00d2be3c3
bakeds <bakeds@gmail.com>
2014-07-10 20:23:18 +0200

    Combinatie toegevoegd nog niet getest

---
+++ MathBarld/Combination.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBarld
+{
+    public class Combination
+    {
+        public int Number { get; set; }
+        public int K { get; set; }
+
+        public int Value
+        {
+            get
+            {
+                return (Faculteit.GetIteratiev(Number)) / (Faculteit.GetIteratiev(K) * Faculteit.GetIteratiev(Number - K));
+            }
+        }
+
+        public Combination(int number, int combination)
+        {
+            K = combination;
+            Number = number;
+        }
+
+
+    }
+}
---
+++ MathBarld/ProjectEuler/Problem3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathBarld;
+
+namespace MathBarld.ProjectEuler
+{
+    public class Problem3 : MathBarld.ProjectEuler.IProblem
+    {
+        public string ProblemDescription
+        {
+            get
+            {
+                return @"The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?";
+            }
+        }
+
+        public int GetAnswer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
