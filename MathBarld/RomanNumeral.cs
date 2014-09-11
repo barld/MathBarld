454824a00593cecf336eedfd88ac76a94ad5a239
barld <bakeds@gmail.com>
2014-09-11 23:25:29 +0200

    begin voor object romeins cijfer

---
+++ MathBarld/RomanNumeral.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBarld
+{
+    public class RomanNumeral
+    {
+        #region private fields
+        /// <remarks>
+        /// start with an emtpty string because there is no 0
+        /// </remarks>
+        string _romanNumber = "";
+        int _decimalNumber = 0;
+        #endregion
+
+        #region public properties
+        public int Decimal { get { return this._decimalNumber; } }
+        #endregion
+
+        #region operator overloading
+        public static RomanNumeral operator +(RomanNumeral a, RomanNumeral b)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+
+        #region overide object methods
+        public override string ToString()
+        {
+            return this._romanNumber;
+        }
+
+        #endregion
+    }
+}
