7e700cd32123036ae91fe4731f503052fa015dc9
Barld <Bakeds@gmail.com>
2014-10-02 23:06:53 +0200

    overgestapt naar IList

--- Other/BarldList.cs
+++ Other/BarldList.cs
@@ -10,18 +10,99 @@ namespace Other
     /// <summary>
     /// own wrapper on array
     /// </summary>
-    class BarldList<T> : IEnumerable<T>
+    public class BarldList<T> : IList<T>
     {
         private T[] _collection;
 
+        #region constructors
+        public BarldList() :this(new T[0])
+        {
+
+        }
+
+        public BarldList(IEnumerable<T> collection)
+        {
+            _collection = collection.ToArray();
+        }
+        #endregion
+
+        public T this[int index]
+        {
+            get
+            {
+                return _collection[index];
+            }
+
+            set
+            {
+                _collection[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
         public IEnumerator<T> GetEnumerator()
         {
-            return this._collection.GetEnumerator() as IEnumerator<T>;
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return this._collection.GetEnumerator();
+            throw new NotImplementedException();
         }
     }
 }
