1b450ec985bf8cf0bae3c9d45a19dc758ecb2e84
Barld <Bakeds@gmail.com>
2014-10-02 22:37:46 +0200

    add library for other stuff

---
+++ Other/BarldList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Other
+{
+    /// <summary>
+    /// own wrapper on array
+    /// </summary>
+    class BarldList<T> : IEnumerable<T>
+    {
+        private T[] _collection;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this._collection.GetEnumerator() as IEnumerator<T>;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this._collection.GetEnumerator();
+        }
+    }
+}
