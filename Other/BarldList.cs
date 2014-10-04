0e31070768a52db605606c8303c559f9045197c5
Barld <Bakeds@gmail.com>
2014-10-04 14:00:15 +0200

    Revert "overgestapt naar IList"
    This reverts commit 7e700cd32123036ae91fe4731f503052fa015dc9.

--- Other/BarldList.cs
+++ Other/BarldList.cs
@@ -10,99 +10,18 @@ namespace Other
     /// <summary>
     /// own wrapper on array
     /// </summary>
-    public class BarldList<T> : IList<T>
+    class BarldList<T> : IEnumerable<T>
     {
         private T[] _collection;
 
-        #region constructors
-        public BarldList() :this(new T[0])
-        {
-
-        }
-
-        public BarldList(IEnumerable<T> collection)
-        {
-            _collection = collection.ToArray();
-        }
-        #endregion
-
-        public T this[int index]
-        {
-            get
-            {
-                return _collection[index];
-            }
-
-            set
-            {
-                _collection[index] = value;
-            }
-        }
-
-        public int Count
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public bool IsReadOnly
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public void Add(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Contains(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void CopyTo(T[] array, int arrayIndex)
-        {
-            throw new NotImplementedException();
-        }
-
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
-        }
-
-        public int IndexOf(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Insert(int index, T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool Remove(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveAt(int index)
-        {
-            throw new NotImplementedException();
+            return this._collection.GetEnumerator() as IEnumerator<T>;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return this._collection.GetEnumerator();
         }
     }
 }
