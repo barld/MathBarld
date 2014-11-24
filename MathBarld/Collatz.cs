9fc7222d0a697699b886e80bc0f562e842e1ddf8
barld <bakeds@gmail.com>
2014-11-24 08:23:11 +0100

    recursiefe methode voor Collatz toegevoegd

---
+++ MathBarld/Collatz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBarld
+{
+    public static class Collatz
+    {
+        static Dictionary<int, int> collatzCountCollections = new Dictionary<int, int>();
+        public static int GetCollectionCountRecursief(int n)
+        {
+            if (n == 1)
+                return 1;
+            else if (collatzCountCollections.ContainsKey(n))
+                return collatzCountCollections[n];
+            else
+            {
+                int nl;
+                if (n % 2 == 0)
+                    nl = GetCollectionCountRecursief(n / 2);
+                else
+                    nl = GetCollectionCountRecursief(3 * n + 1);
+
+                nl++;
+                collatzCountCollections[n] = nl;
+
+                return nl;
+            }
+        }
+    }
+}
---
+++ MathBarldTest/CollatzTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathBarldTest
+{
+    [TestClass]
+    public class CollatzTests
+    {
+        [TestMethod]
+        public void CollectionOf13Test()
+        {
+            var c = MathBarld.Collatz.GetCollectionCountRecursief(13);
+
+            Assert.AreEqual<int>(c, 10);
+        }
+    }
+}
