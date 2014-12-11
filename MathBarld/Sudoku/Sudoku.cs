3c24155f1da9ef88ffa514f5c0c3c6caccc018a9
barld <bakeds@gmail.com>
2014-12-11 21:42:46 +0100

    add empty classe for solving sudoku's

---
+++ MathBarld/Sudoku/Sudoku.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBarld.Sudoku
+{
+    public class Sudoku
+    {
+    }
+}
